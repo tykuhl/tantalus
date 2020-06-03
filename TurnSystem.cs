51270690247a6ab7adbf7afc9080a2b309f22739
tykuhl <34179928+tykuhl@users.noreply.github.com>
2020-06-03 10:07:35 -0500

    Add files via upload

---
+++ TurnSystem.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TurnSystem : MonoBehaviour
+{
+    IList<Unit> mylist = new List<Unit>();
+
+    public void setupTurnSystem(IList<Unit> u)
+    {
+        foreach (var x in u)
+        {
+            mylist.Add(x);
+        }
+
+        showList(mylist);
+    }
+
+    public void showList(IList<Unit> l)
+    {
+        foreach (var x in l)
+        {
+            Debug.Log(x.unitName + ", Speed: " + x.speed + ", Current HP: " + x.currentHP);
+        }
+    }
+
+}
