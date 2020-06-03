f054f7ca408a9a7ca491ea2e7286239c594c1eab
tykuhl <34179928+tykuhl@users.noreply.github.com>
2020-06-03 15:41:56 -0500

    Update Unit.cs

--- Unit.cs
+++ Unit.cs
@@ -1,4 +1,4 @@
-﻿using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -12,6 +12,7 @@ public class Unit : MonoBehaviour
     public int maxHP;
     public int currentHP;
     public int speed;
+    public bool isEnemy;
 
     public bool TakeDamage(int dmg)
     {
