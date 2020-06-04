a68cda6558d376e9ba4da9e79abcfd4a88d35523
tykuhl <34179928+tykuhl@users.noreply.github.com>
2020-06-03 21:57:51 -0500

    Add files via upload

---
+++ BattleEncounter.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum BattleEnvironmentEnum { WOODS, SNOW, MOUNTAIN, HOUSE, CASTLE }
+
+public class BattleEncounter : MonoBehaviour
+{
+    public BattleEnvironmentEnum environment; 
+    
+    IList<Unit> playerUnits = new List<Unit>();
+    IList<Unit> enemyUnits = new List<Unit>();
+
+}
