60d0316f9fb8132433b5a99867da86c556fcecee
tykuhl <34179928+tykuhl@users.noreply.github.com>
2020-06-12 00:01:29 -0500

    Add files via upload

---
+++ UnitManager.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UnitManager : MonoBehaviour
+{
+    public Unit data;
+
+    public bool TakeDamage(int dmg)
+    {
+        data.currentHP -= dmg;
+
+        if (data.currentHP <= 0)
+            return true;
+        else
+            return false;
+    }
+
+    public void HealDamage(int dmg)
+    {
+        // if healing is more than max hp, heal only partial amount
+        if ((dmg + data.currentHP) > data.maxHP)
+            data.currentHP = data.maxHP;
+        else if (dmg + data.currentHP <= data.maxHP)
+            data.currentHP += dmg;
+    }
+}
